ccc8a6cc297f1456a963a3686aaa344fbafcf19f
rohseol <seven800@naver.com>
2022-10-17 11:05:59 +0900

    uncomment

--- src/main/java/com/classMetabus/web/Admin/service/MessageService.java
+++ src/main/java/com/classMetabus/web/Admin/service/MessageService.java
@@ -38,33 +38,33 @@ public class MessageService {
     @Value("${icns.app.coolsms.phone}")
     private String phone;
 
-//    public Boolean sendMessage(SendMessageRequest request){
-//        Optional<StudentList> stuList = studentListRepository.findIdByStudent_IdAndLecture_id(request.getStudentId(), request.getLectureId());
-//
-//        if (stuList.isPresent()){
-//            String toPhone = stuList.get().getStudent().getPhone();
-//            String studentName = stuList.get().getStudent().getName();
-//            String lectureName = stuList.get().getLecture().getName();
-//            Message coolsms = new Message(apiKey, apiSecret);
-//
-//            HashMap<String,String> params = new HashMap<String,String>();
-//            params.put("to",toPhone);
-//            params.put("from",phone); // 사전에 사이트에서 번호를 인증하고 등록해야함
-//            params.put("type","SMS");
-//            params.put("text",studentName+"님 '"+lectureName+"' 과목의 수강신청이 완료 되었습니다."); // 메세지 내용
-//            //params.put("app_version","test app1.2");
-//
-//            System.out.println("전송완료");
-////            try{
-////                JSONObject obj = (JSONObject) coolsms.send(params);
-////                System.out.println(obj.toString()); // 전송 결과 출력
-////            }catch (CoolsmsException e){
-////                System.out.println(e.getMessage());
-////                System.out.println(e.getCode());
-////            }
-//            return true;
-//        }else {return false;}
-//    }
+    public Boolean sendMessage(SendMessageRequest request){
+        Optional<StudentList> stuList = studentListRepository.findIdByStudent_IdAndLecture_id(request.getStudentId(), request.getLectureId());
+
+        if (stuList.isPresent()){
+            String toPhone = stuList.get().getStudent().getPhone();
+            String studentName = stuList.get().getStudent().getName();
+            String lectureName = stuList.get().getLecture().getName();
+            Message coolsms = new Message(apiKey, apiSecret);
+
+            HashMap<String,String> params = new HashMap<String,String>();
+            params.put("to",toPhone);
+            params.put("from",phone); // 사전에 사이트에서 번호를 인증하고 등록해야함
+            params.put("type","SMS");
+            params.put("text",studentName+"님 '"+lectureName+"' 과목의 수강신청이 완료 되었습니다."); // 메세지 내용
+            //params.put("app_version","test app1.2");
+
+            System.out.println("전송완료");
+//            try{
+//                JSONObject obj = (JSONObject) coolsms.send(params);
+//                System.out.println(obj.toString()); // 전송 결과 출력
+//            }catch (CoolsmsException e){
+//                System.out.println(e.getMessage());
+//                System.out.println(e.getCode());
+//            }
+            return true;
+        }else {return false;}
+    }
     /*public List<String> sendMessage(String name, String context){
         List<Student> studentList = studentLoginRepository.findAll();
         List<String> phoneList = new ArrayList<>();
